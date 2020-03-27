d23412339ab4048ccddcf12c1c9caed913106269
Wei How Owi <33288994+howowi@users.noreply.github.com>
2020-03-27 17:42:58 +0800

    Update add_sshkey.sh

--- add_sshkey.sh
+++ add_sshkey.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 ## remove the public_sshkey variable first
-sed '/variable "public*/,/}/d' 
+sed '/variable "public*/,/}/d' instance_config.tf > instance_config.tf
 
 public_key=$(cat ~/.ssh/id_rsa.pub)
 
