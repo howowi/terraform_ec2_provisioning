7c54aff97ee8d92ba27bb6e019eee3d93fca39bb
Wei How Owi <33288994+howowi@users.noreply.github.com>
2020-03-27 17:55:34 +0800

    Update add_sshkey.sh

--- add_sshkey.sh
+++ add_sshkey.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
 ## remove the public_sshkey variable first
-sed '/variable "public*/,/}/d' instance_config.tf > instance_config.tf
+sed '/variable "public*/,/}/d' instance_config.tf > instance_config_tmp.tf
+mv instance_config_tmp.tf instance_config.tf
 
+## store the public SSH key from the default directory to the instance_config.tf
 public_key=$(cat ~/.ssh/id_rsa.pub)
-
-cat <<EOT >> test.yml
+cat <<EOT >> instance_config.tf
 variable "public_sshkey" {
   default = "$public_key"
 }
